593fe4f05ef05dde70d6cccfaafac40def72a22b
xuexiaojian <xiaojianxue@gmail.com>
2014-05-07 09:53:03 +0800

    新建分支，用来自己测试数据库操作

--- src/com/bupt/bnrc/thesenser/dao/DAOHelper.java
+++ src/com/bupt/bnrc/thesenser/dao/DAOHelper.java
@@ -18,6 +18,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public abstract class DAOHelper extends SQLiteOpenHelper implements DatabaseConstants {
 
+	// test datebase
 	protected static final String TEST_TABLE_NAME = "test";
     protected static final String TEST_NAME = "name";
     protected static final String TEST_INT = "int";
@@ -34,6 +35,37 @@ public abstract class DAOHelper extends SQLiteOpenHelper implements DatabaseCons
     		TEST_FLOAT + "FLOAT," +
     		TEST_DOUBLE + "DOUBLE" + 
     		");";
+    
+    // file datebase
+    protected static final String FILE_TABLE_NAME = "files"; 
+    protected static final String FILE_FILE_NAME = "file_name";
+    protected static final String FILE_CREATE_TIME = "create_time";
+    protected static final String FILE_X_DIRECT = "x_direct";
+    protected static final String FILE_Y_DIRECT = "y_direct";
+    protected static final String FILE_Z_DIRECT = "z_direct";
+    protected static final String FILE_LONGITUDE = "longitude";
+    protected static final String FILE_LATITUDE = "latitude";
+    protected static final String FILE_EXPOSURE_VALUE = "exposure_value";
+    protected static final String FILE_FOCAL_DISTANCE = "focal_distance";
+    protected static final String FILE_APERTURE = "aperture";
+    protected static final String[] FILE_ALL_COLUMS = { _ID, FILE_FILE_NAME, FILE_CREATE_TIME, FILE_X_DIRECT, 
+    	FILE_Y_DIRECT, FILE_Z_DIRECT, FILE_LONGITUDE, FILE_LATITUDE, FILE_EXPOSURE_VALUE, FILE_FOCAL_DISTANCE,
+    	FILE_APERTURE };
+    private static final String FILE_TABLE_CREATE = 
+    		"CREATE TABLE" + FILE_TABLE_NAME + "(" +
+    		_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + 
+    		FILE_FILE_NAME + "NTEXT NOT NULL, " +
+    		FILE_CREATE_TIME + "DATE NOT NULL, " +
+    		FILE_X_DIRECT + "FLOAT, " + 
+    		FILE_Y_DIRECT + "FLOAT, " + 
+    		FILE_Z_DIRECT + "FLOAT, " +
+    		FILE_LONGITUDE + "FLOAT, " + 
+    		FILE_LATITUDE + "FLOAT, " + 
+    		FILE_EXPOSURE_VALUE + "INTEGER, " + 
+    		FILE_FOCAL_DISTANCE + "FLOAT, " + 
+    		FILE_APERTURE + "FLOAT" +
+    		");";
+    		
 	
 	public DAOHelper(Context context, String name, CursorFactory factory,
 			int version) {
@@ -45,6 +77,7 @@ public abstract class DAOHelper extends SQLiteOpenHelper implements DatabaseCons
 	public void onCreate(SQLiteDatabase db) {
 		// TODO Auto-generated method stub
 		db.execSQL(TEST_TABLE_CREATE);
+		db.execSQL(FILE_TABLE_CREATE);
 		
 	}
 	
@@ -52,6 +85,7 @@ public abstract class DAOHelper extends SQLiteOpenHelper implements DatabaseCons
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub
 		db.execSQL("DROP TABLE IF EXISTS " + TEST_TABLE_NAME);
+		db.execSQL("DROP TABLE IF EXISTS " + FILE_TABLE_NAME);
 		onCreate(db);
 	}
 	
