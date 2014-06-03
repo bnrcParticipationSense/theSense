a2e85e3d4522697c1225310aba2944a8728d704f
xuexiaojian <xiaojianxue@gmail.com>
2014-06-03 09:17:10 +0800

    修改为抽屉式窗口

--- gen/com/bupt/bnrc/thesenser/R.java
+++ gen/com/bupt/bnrc/thesenser/R.java
@@ -8,6 +8,11 @@
 package com.bupt.bnrc.thesenser;
 
 public final class R {
+    public static final class array {
+        /**  main 
+         */
+        public static final int main_list_array=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -22,70 +27,82 @@ public final class R {
     }
     public static final class drawable {
         public static final int capture=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_drawer=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f08001d;
-        public static final int cameraBtn=0x7f08000e;
-        public static final int collectBtn=0x7f080011;
-        public static final int dataTestBtn=0x7f08000f;
-        public static final int data_lookListBtn=0x7f080015;
-        public static final int data_lookOneBtn=0x7f080014;
-        public static final int exitBtn=0x7f080013;
-        public static final int fileTestBtn=0x7f080010;
-        public static final int file_lookListBtn=0x7f080003;
-        public static final int get_id=0x7f080016;
-        public static final int get_id_file=0x7f080018;
-        public static final int get_list=0x7f080017;
-        public static final int get_list_file=0x7f080019;
-        public static final int helloText=0x7f08000d;
-        public static final int info=0x7f080001;
-        public static final int listData_details=0x7f080005;
-        public static final int listData_next_btn=0x7f080007;
-        public static final int listData_pre_btn=0x7f080006;
-        public static final int listData_title=0x7f080004;
-        public static final int listFile_details=0x7f08000a;
-        public static final int listFile_next_btn=0x7f08000c;
-        public static final int listFile_pic=0x7f080009;
-        public static final int listFile_pre_btn=0x7f08000b;
-        public static final int listFile_title=0x7f080008;
-        public static final int phone_name=0x7f08001b;
-        public static final int photoBut=0x7f080002;
-        public static final int photo_name=0x7f08001a;
-        public static final int sView=0x7f080000;
-        public static final int saveInfo=0x7f080012;
-        public static final int show=0x7f08001c;
+        public static final int action_settings=0x7f090020;
+        public static final int cameraBtn=0x7f090011;
+        public static final int collectBtn=0x7f090014;
+        public static final int content_frame=0x7f09000e;
+        public static final int dataTestBtn=0x7f090012;
+        public static final int data_lookListBtn=0x7f090018;
+        public static final int data_lookOneBtn=0x7f090017;
+        public static final int exitBtn=0x7f090016;
+        public static final int fileTestBtn=0x7f090013;
+        public static final int file_lookListBtn=0x7f090003;
+        public static final int get_id=0x7f090019;
+        public static final int get_id_file=0x7f09001b;
+        public static final int get_list=0x7f09001a;
+        public static final int get_list_file=0x7f09001c;
+        public static final int helloText=0x7f090010;
+        public static final int info=0x7f090001;
+        public static final int listData_details=0x7f090005;
+        public static final int listData_next_btn=0x7f090007;
+        public static final int listData_pre_btn=0x7f090006;
+        public static final int listData_title=0x7f090004;
+        public static final int listFile_details=0x7f09000a;
+        public static final int listFile_next_btn=0x7f09000c;
+        public static final int listFile_pic=0x7f090009;
+        public static final int listFile_pre_btn=0x7f09000b;
+        public static final int listFile_title=0x7f090008;
+        public static final int main_drawer_layout=0x7f09000d;
+        public static final int main_left_drawer=0x7f09000f;
+        public static final int phone_name=0x7f09001e;
+        public static final int photoBut=0x7f090002;
+        public static final int photo_name=0x7f09001d;
+        public static final int sView=0x7f090000;
+        public static final int saveInfo=0x7f090015;
+        public static final int show=0x7f09001f;
     }
     public static final class layout {
         public static final int activity_camera=0x7f030000;
         public static final int activity_file=0x7f030001;
         public static final int activity_list_data=0x7f030002;
         public static final int activity_list_file=0x7f030003;
-        public static final int activity_test=0x7f030004;
-        public static final int antivity_data=0x7f030005;
-        public static final int get_data_id=0x7f030006;
-        public static final int get_data_list=0x7f030007;
-        public static final int get_file_id=0x7f030008;
-        public static final int get_file_list=0x7f030009;
-        public static final int save=0x7f03000a;
+        public static final int activity_main=0x7f030004;
+        public static final int activity_test=0x7f030005;
+        public static final int antivity_data=0x7f030006;
+        public static final int get_data_id=0x7f030007;
+        public static final int get_data_list=0x7f030008;
+        public static final int get_file_id=0x7f030009;
+        public static final int get_file_list=0x7f03000a;
+        public static final int main_drawer_list_item=0x7f03000b;
+        public static final int save=0x7f03000c;
     }
     public static final class menu {
-        public static final int test=0x7f070000;
+        public static final int test=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
+        /**  project 
+         */
         public static final int app_name=0x7f050000;
-        public static final int camera_helloText=0x7f050006;
-        public static final int data_lookListBtn=0x7f05000a;
-        public static final int data_lookOneBtn=0x7f050009;
-        public static final int data_saveBtn=0x7f050008;
-        public static final int file_lookListBtn=0x7f05000c;
-        public static final int file_lookOneBtn=0x7f05000b;
-        public static final int photo_name=0x7f050007;
-        public static final int test_cameraBtn=0x7f050003;
-        public static final int test_dataTestBtn=0x7f050004;
-        public static final int test_fileTestBtn=0x7f050005;
-        public static final int test_helloText=0x7f050002;
+        public static final int camera_helloText=0x7f050008;
+        public static final int data_lookListBtn=0x7f05000c;
+        public static final int data_lookOneBtn=0x7f05000b;
+        public static final int data_saveBtn=0x7f05000a;
+        public static final int drawer_close=0x7f050003;
+        public static final int drawer_open=0x7f050002;
+        public static final int file_lookListBtn=0x7f05000e;
+        public static final int file_lookOneBtn=0x7f05000d;
+        public static final int photo_name=0x7f050009;
+        public static final int test_cameraBtn=0x7f050005;
+        public static final int test_dataTestBtn=0x7f050006;
+        public static final int test_fileTestBtn=0x7f050007;
+        /**  test 
+         */
+        public static final int test_helloText=0x7f050004;
     }
     public static final class style {
         /** 
@@ -109,10 +126,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
